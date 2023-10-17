653bdede5211a53dc1e4fb83df89d99ec2345ef7
unknown <robbysakti2399@gmail.com>
2023-10-17 15:16:57 +0700

    copy 1

---
+++ server/index.js
@@ -0,0 +1,50 @@
+//import module
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const port = 3001;
+
+// connection mongoDB
+const connectDB = require('./database/db');
+connectDB();
+
+//import router
+// const categoryRouter = require('./router/categoryRouter');
+
+//setting cors, morgan
+app.use(cors());
+app.use(logger('dev'));
+//setup post
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Cors
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Authorization, authorization, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Cache-Control",
+      "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+    );
+    next();
+});
+
+//setup public url for file
+app.use(express.static(path.join(__dirname, 'public')));
+
+//url
+// app.use('/api/category', categoryRouter);
+
+//client
+// app.use('/api/client', homeRouter);
+
+
+app.listen(port, () => {
+    console.log(`app listening to http://localhost:${port}`);
+});
